3ae70d6f74cd8035a64de732f1bf4d28aec2abac
William Rodriguez <wiarodriguezmo@unal.edu.co>
2020-11-27 12:49:20 -0500

    Script SQL para Cliente en MySQL

---
+++ script.sql
@@ -0,0 +1,9 @@
+CREATE  TABLE `monitorias`.`clientes` (
+  `idclientes` INT NOT NULL AUTO_INCREMENT ,
+  `nombres` VARCHAR(85) NOT NULL ,
+  `email` VARCHAR(45) NULL ,
+  `telefono` VARCHAR(15) NULL ,
+  PRIMARY KEY (`idclientes`) );
+
+
+
